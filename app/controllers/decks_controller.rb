321a9517349a5fdb7fba1e27258a0e153af3f6d7
zcammack <zack.cammack@gmail.com>
2020-11-06 14:33:55 -0600

    Added index, new, and create actions in Decks controller. Create action wants to associate decks to users but will be unfinished for now. Need to figure out appropriate pathing first.

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,2 +1,28 @@
 class DecksController < ApplicationController
+    require 'faraday'
+    require 'json'
+
+    $set = MTG::Set.all
+    $type = MTG::Type.all
+    $creaturetype = MTG::Subtype.all
+
+    def index
+        @decks = Deck.all
+    end
+
+    def new
+        @deck = Deck.new
+    end
+
+    def create
+        @user = current_user
+        deck = Deck.new(deck_params)
+        if deck.save
+            redirect_to 
+        else
+            redirect_to 
+        end
+    end
+
+
 end
