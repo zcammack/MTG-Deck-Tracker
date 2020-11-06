438157002a6d987b2184eec07e13a7bd5c9d27f5
zcammack <zack.cammack@gmail.com>
2020-11-06 14:31:26 -0600

    Added some logic to the ApplicationController and DecksController to make use of the MTGsdk gem. This should allow the application to access official MTG cards information.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    MTG.configure do |config|
+        config.api_version = 1
+    end
+
+    def after_sign_in_path_for(resource)
+        user_profile_path(current_user)
+    end
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,2 +1,4 @@
 class Deck < ApplicationRecord
+    belongs_to :user
+    belongs_to :card
 end
